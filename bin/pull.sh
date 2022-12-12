0c475bfb055847770afc19145eb64ccc77a64e62
Frédéric Glorieux <frederic.glorieux@fictif.org>
2022-12-12 16:34:56 +0100

    to update subtree /xsl/

--- bin/pull.sh
+++ bin/pull.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 dir=`dirname -- "$0"`
 pushd $dir/..
-git subtree pull --prefix src/xsl https://github.com/oeuvres/teinte_xsl --squash
+git subtree pull --prefix src/xsl https://github.com/oeuvres/teinte_xsl main --squash
 popd
