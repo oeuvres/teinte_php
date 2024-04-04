ad2244283f88b0b04e38fe8255a026bf617f6c61
Frédéric Glorieux <frederic.glorieux@fictif.org>
2024-04-04 15:03:20 +0200

    CliGlob API changes

--- examples/docx_tei.php
+++ examples/docx_tei.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-include_once(dirname(__DIR__) . '/vendor/autoload.php');
+include_once(dirname(__DIR__ ) . '/vendor/autoload.php');
 
 use Psr\Log\LogLevel;
 use Oeuvres\Kit\{Cliglob, Filesys, Log, Parse, Xt};
@@ -9,14 +9,16 @@ use Oeuvres\Teinte\Format\{Docx, Tei};
 
 class Docx2Tei extends Cliglob {
     private static Docx $docx;
-    const SRC_FORMAT = "docx";
-    const SRC_EXT = ".docx";
-    const DST_FORMAT = "xml";
-    const DST_EXT = ".xml";
 
     /** work */
     static public function cli()
     {
+        // requested destination extension
+        self::put('dst_ext', ".xml");
+        // doc for help
+        self::put('dst_ext', ".xml"); 
+        self::put('src_format', "DOCX");
+        self::put('dst_format', "XML/TEI");
         self::$docx = new Docx();
         Log::setLogger(new LoggerCli(LogLevel::DEBUG));
         self::glob([__CLASS__, 'export']);
@@ -26,7 +28,7 @@ class Docx2Tei extends Cliglob {
     {
         Log::info($src_file . " > " . $dst_file);
         $pars = [];
-        if (isset(self::$options['t'])) $pars['template.xml'] = self::$options['t'];
+        $pars['template.xml'] = self::get('t', null);
         self::$docx->open($src_file);
         self::$docx->teiURI($dst_file);
     }
